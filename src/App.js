1598878b5540c42a93af682549c7805ce017bf39
sukanya1509 <namsukanya1509@gmail.com>
2020-09-10 04:10:22 +0700

    2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import CharacterCard from './CharacterCard';
+import WordCard from './WordCard';
+import './App.css';
+
 function App() {
- return (
- <div>
- <CharacterCard value="h"/>
- <CharacterCard value="i"/>
- </div>
- );
+  return (
+    <div>
+    <WordCard value="hello"/>
+    </div>
+    );
 }
 
 export default App;
